41d492dccb4096651f093bf5888f1276899148d8
SamHarron <harronsam@gmail.com>
2021-07-16 14:23:02 -0400

    added validations to the employee model

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,10 @@
 class Employee < ApplicationRecord
     has_secure_password
+
+    validates :username, presence: true
+    validates :username, uniqueness: true
+
+    validates :email, presence: true
+    validates :email, uniqueness: true
+
 end
