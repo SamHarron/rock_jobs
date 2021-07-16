d7c49e42263ffd9614a54ef97415d581adc68c3a
SamHarron <harronsam@gmail.com>
2021-07-16 10:24:11 -0400

    added create action to the employees controller

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -4,6 +4,18 @@ class EmployeesController < ApplicationController
         @employee = Employee.new
     end
 
+    def create 
+       @employee = Employee.create(employee_params)
+       if @employee.save
+        render :home
+       else
+        render :new
+       end
+    end
 
+    private
 
+    def employee_params
+        params.require(:employee).permit(:username, :email, :password, :password_confirmation)
+    end
 end
