f75680b74dc4d0edaf521d205da0f72ddf226e4b
SamHarron <harronsam@gmail.com>
2021-07-18 01:07:38 -0400

    added application helper methods, updated the application navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+
+    helper_method :current_user, :logged_in?
+
+    def current_user
+        if session[:employee_id]
+            Employee.find(session[:employee_id])
+        end
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+
 end
