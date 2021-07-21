aaa5054ea434078b5a3e27b6ef80a2a64a071ddb
SamHarron <harronsam@gmail.com>
2021-07-20 23:36:58 -0400

    updated location validations

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,7 @@
 class Location < ApplicationRecord
     belongs_to :employee
 
-    validates :street_address, presence: true
+    validates :street_address, presence: true, uniqueness: true 
     validates :city, presence: true
     validates :state, presence: true
     validates :zip_code, presence: true
