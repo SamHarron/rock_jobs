64c3aa2f501c24ca134c6cae1db73e1630c897e2
SamHarron <harronsam@gmail.com>
2021-07-22 22:03:01 -0400

    started creating nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,23 @@
 Rails.application.routes.draw do
 
-  resources :locations
-  resources :employees
-
   get '/signup', to: 'employees#new'
+  post '/signup', to: 'employees#create'
 
 	get '/login', to: 'sessions#new'
 	post '/login', to: 'sessions#create'
 	
   delete '/logout', to: 'sessions#logout', as: 'logout'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  resources :employees do
+    resources :locations, only: [:index]
+  end
 
+  resources :locations do
+    resources :measurements, only: [:new, :create, :index]
+  end
+
+  resources :measurements
 
 end
+
