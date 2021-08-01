676369e32e1c10c7f30f044e623e6c090957240b
SamHarron <harronsam@gmail.com>
2021-08-01 02:49:54 -0400

    added a helper for the location index view

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -1,2 +1,12 @@
 module LocationsHelper
+
+    def employees_locations
+        if @employee
+            "#{@employee.username}'s Locations"
+        else
+            'Please Sign In or Sign Up'
+        end
+    end
+
+
 end
